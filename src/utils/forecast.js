89846ad6705f02ee9649c337571f4e0a347b8c9e
VWRoli <fustroli@gmail.com>
2021-05-20 09:24:59 +0200

    add humidity data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require("request");
 
 const forecast = (lng, lat, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=b10c5083f708da4d93eb7717ddbc3d99&query=${lat},${lng}&units=m`;
@@ -7,14 +7,16 @@ const forecast = (lng, lat, callback) => {
     if (error) {
       callback(`Unable to connect to weather service!`, undefined);
     } else if (response.body.error) {
-      callback('Unable to find location', undefined);
+      callback("Unable to find location", undefined);
     } else {
       const temp = response.body.current.temperature;
       const feels = response.body.current.feelslike;
       const desc = response.body.current.weather_descriptions[0];
+      const humidity = response.body.current.humidity;
       callback(
         undefined,
-        `${desc}. It is currently ${temp} degrees out. It feels like ${feels} degrees out.`
+        `${desc}. It is currently ${temp} degrees out. It feels like ${feels} degrees out. 
+        The humidity is ${humidity}%.`
       );
     }
   });
