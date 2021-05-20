ed5cf431dab5c48824c3f395496631bab0cdee0e
VWRoli <fustroli@gmail.com>
2021-05-20 08:53:49 +0200

    Setup app for heroku

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -1,64 +1,65 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('hbs');
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const forecast = require('./utils/forecast');
-const geocode = require('./utils/geocode');
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 //console.log(__dirname);
 //console.log(path.join(__dirname, '../public'));
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Weather App',
-    name: 'Roland',
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Roland",
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About me',
-    name: 'Roland',
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Roland",
   });
 });
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    msg: 'This is a sample message!',
-    title: 'Help',
-    name: 'Roland',
+app.get("/help", (req, res) => {
+  res.render("help", {
+    msg: "This is a sample message!",
+    title: "Help",
+    name: "Roland",
   });
 });
 
 //404 page handling happens after we set up all the other routes
 
-app.get('/help/*', (req, res) => {
-  res.render('404page', {
-    msg: 'Help article not found',
-    title: 'Help',
-    name: 'Roland',
+app.get("/help/*", (req, res) => {
+  res.render("404page", {
+    msg: "Help article not found",
+    title: "Help",
+    name: "Roland",
   });
 });
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: 'You must provide a valid address',
+      error: "You must provide a valid address",
     });
   }
   geocode(
@@ -85,10 +86,10 @@ app.get('/weather', (req, res) => {
   //res.send({ loaction: 'Malta', forecast: '25C', address: req.query.address });
 });
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term',
+      error: "You must provide a search term",
     });
   }
   console.log(req.query);
@@ -97,14 +98,14 @@ app.get('/products', (req, res) => {
   });
 });
 
-app.get('*', (req, res) => {
-  res.render('404page', {
-    msg: 'Page not found',
-    title: '404',
-    name: 'Roland',
+app.get("*", (req, res) => {
+  res.render("404page", {
+    msg: "Page not found",
+    title: "404",
+    name: "Roland",
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server started on 3000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
