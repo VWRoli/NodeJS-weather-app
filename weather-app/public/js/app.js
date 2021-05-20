f63a06b9520e6cb8fa04128982305f6df7ecc48f
VWRoli <fustroli@gmail.com>
2021-05-20 08:19:39 +0200

    weather-app init

---
+++ weather-app/public/js/app.js
@@ -0,0 +1,36 @@
+console.log('Client side JS file is loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+const fetchForecast = (location) => {
+  //classes
+  messageOne.classList.remove('error');
+  messageOne.classList.remove('success');
+  messageOne.classList.add('loading');
+  messageOne.textContent = 'Loading...';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.classList.remove('loading');
+          messageOne.classList.add('error');
+          return (messageOne.textContent = data.error);
+        }
+        messageOne.classList.remove('loading');
+        messageOne.classList.add('success');
+        messageOne.textContent = data.location + ' ' + data.forecast;
+      });
+    }
+  );
+};
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  fetchForecast(location);
+});
---
+++ weather-app/src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const forecast = (lng, lat, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=b10c5083f708da4d93eb7717ddbc3d99&query=${lat},${lng}&units=m`;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback(`Unable to connect to weather service!`, undefined);
+    } else if (response.body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      const temp = response.body.current.temperature;
+      const feels = response.body.current.feelslike;
+      const desc = response.body.current.weather_descriptions[0];
+      callback(
+        undefined,
+        `${desc}. It is currently ${temp} degrees out. It feels like ${feels} degrees out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
